4c71942b127b76c8f88030fe862260104f846786
Buddhi Rangana <buddhirangana1999@gmail.com>
2026-02-16 20:17:11 +0530

    remove unwonted spaces

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import productRouter from "./routers/productRouter.js";
 
 const app = express();
 
-
 const mongodbURI = "mongodb+srv://admin:1234@cluster0.xuujurc.mongodb.net/icomputers?appName=Cluster0"
 
 mongoose.connect(mongodbURI).then(
@@ -19,10 +18,8 @@ app.use( express.json() )
 
 app.use(authenticateUser)
 
-
 app.use("/users",userRouter)
 app.use("/products",productRouter)
-
 
 app.listen(3000, (req,res) => {
     console.log("Server is running on port 3000");
