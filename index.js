957ea617bae242f22b41b9e9cc5b59b00cb9ea5d
Buddhi Rangana <buddhirangana1999@gmail.com>
2026-03-09 21:02:30 +0530

    fix: update API routes to include prefix for consistency

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.use( express.json() )
 
 app.use(authenticateUser)
 
-app.use("/users",userRouter)
-app.use("/products",productRouter)
+app.use("/api/users",userRouter)
+app.use("/api/products",productRouter)
 
 app.listen(3000, (req,res) => {
     console.log("Server is running on port 3000");
